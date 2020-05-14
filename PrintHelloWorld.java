fac5c799ec370e2cb0f8bbac1c6eee8121e97192
vaikhe27 <vaishalikhetre2712@gmail.com>
2020-05-14 14:05:13 +0530

    new class added

---
+++ PrintHelloWorld.java
@@ -0,0 +1,26 @@
+package exsersizeToolsQa;
+
+public class PrintHelloWorld {
+
+	public static void main(String[] args) {
+//pritn hello world
+		System.out.println("Hello world");
+		System.out.println("++++++");
+		System.out.println("@@@@@@");
+		System.out.println("******");
+		System.out.println("######");
+		int a;
+		float b;
+		double c;
+		a = 10;
+		b = 20.3f;
+		c = 3.14785;
+		System.out.println("a="+a + "\nb="+ b +"\nc="+ c);
+		boolean b1;
+		b1 = true;
+		b1 = false;
+		System.out.println("boolean value = "+b1);
+	}
+
+}
+
